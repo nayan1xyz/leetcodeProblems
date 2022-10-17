205463c800d413843b360ddccd88ec199c605b93
avd <114290046+avd30@users.noreply.github.com>
2022-10-17 16:30:56 +0530

    created quickSort

--- cpp/quickSort.cpp
+++ cpp/quickSort.cpp
@@ -1,33 +1,60 @@
-def partition (a, start, end):  
-    i = (start - 1)  
-    pivot = a[end] # pivot element  
-      
-    for j in range(start, end):  
-        # If current element is smaller than or equal to the pivot  
-        if (a[j] <= pivot):  
-            i = i + 1  
-            a[i], a[j] = a[j], a[i]  
-      
-    a[i+1], a[end] = a[end], a[i+1]  
+#include <iostream>  
   
-    return (i + 1)  
-      
-# function to implement quick sort   
-def quick(a, start, end): # a[] = array to be sorted, start = Starting index, end = Ending index      
-    if (start < end):  
-        p = partition(a, start, end) # p is partitioning index  
-        quick(a, start, p - 1)  
-        quick(a, p + 1, end)  
+using namespace std;  
   
-          
-def printArr(a): # function to print the array  
-    for i in range(len(a)):  
-        print (a[i], end = " ")  
+/* function that consider last element as pivot,  
+place the pivot at its exact position, and place  
+smaller elements to left of pivot and greater  
+elements to right of pivot.  */  
+int partition (int a[], int start, int end)  
+{  
+    int pivot = a[end]; // pivot element  
+    int i = (start - 1);  
   
+    for (int j = start; j <= end - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (a[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            int t = a[i];  
+            a[i] = a[j];  
+            a[j] = t;  
+        }  
+    }  
+    int t = a[i+1];  
+    a[i+1] = a[end];  
+    a[end] = t;  
+    return (i + 1);  
+}  
+  
+/* function to implement quick sort */  
+void quick(int a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */  
+{  
+    if (start < end)  
+    {  
+        int p = partition(a, start, end);  //p is the partitioning index  
+        quick(a, start, p - 1);  
+        quick(a, p + 1, end);  
+    }  
+}  
+  
+/* function to print an array */  
+void printArr(int a[], int n)  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        cout<<a[i]<< " ";  
+}  
+int main()  
+{  
+    int a[] = { 23, 8, 28, 13, 18, 26 };  
+    int n = sizeof(a) / sizeof(a[0]);  
+    cout<<"Before sorting array elements are - \n";  
+    printArr(a, n);  
+    quick(a, 0, n - 1);  
+    cout<<"\nAfter sorting array elements are - \n";    
+    printArr(a, n);  
       
-a = [68, 13, 1, 49, 58]  
-print("Before sorting array elements are - ")  
-printArr(a)  
-quick(a, 0, len(a)-1)  
-print("\nAfter sorting array elements are - ")  
-printArr(a)  
+    return 0;  
+}  
